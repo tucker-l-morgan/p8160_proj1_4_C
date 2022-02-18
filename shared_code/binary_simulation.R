7dd636e7a310abfb60bf37a2fc12b15ad849953b
Waveley Qiu <waveley.qiu@gmail.com>
2022-02-18 15:59:30 -0500

    adding new binary_simulation file

---
+++ shared_code/binary_simulation.R
@@ -0,0 +1,50 @@
+# ###########
+#
+# Binary Simulation
+#
+# Waveley
+#
+# ###########
+
+source("shared_code/setup.R")
+source("shared_code/data_gen_binary.R")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
